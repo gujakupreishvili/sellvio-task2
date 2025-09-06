3bc67e03573b99b9afd919e7ea9d935245f323b0
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-09-07 03:07:33 +0400

    feat: import content component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Content from "./components/content/content";
 import Header from "./components/header/header";
 
 export default function Home() {
   return (
     <>
    <Header />
-    </>
-    
+   <Content />
+    </> 
   );
 }
