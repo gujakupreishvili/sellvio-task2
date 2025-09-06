a52db578bfe92df85381bc6c7ca7c66afb334960
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-09-07 03:07:48 +0400

    feat:offers component

---
+++ src/app/components/offers/offers.tsx
@@ -0,0 +1,19 @@
+import Image, { StaticImageData } from 'next/image'
+import React from 'react'
+type Props = {
+  title:string,
+  description:string
+  Img:StaticImageData
+}
+
+export default function Offers({title,description,Img}:Props) {
+  return (
+    <div className="bg-[linear-gradient(0deg,#3012B3_0%,#7B62E8_100%)] flex flex-col rounded-[8px] items-center justify-center h-[254px] w-[19%]">
+      <div className='w-[60px] h-[60px] rounded-[30px] bg-white/12 flex items-center justify-center mb-[15px] '>
+        <Image src={Img} alt='' />
+      </div>
+      <h1 className='text-[18px] text-white font-medium font-manrope text-center px-[15px]'>{title}</h1>
+      <p className='text-[#B3B3B3] text-[12px] font-manrope font-medium text-center px-[15px] pt-[8px]'>{description}</p>
+    </div>
+  )
+}
