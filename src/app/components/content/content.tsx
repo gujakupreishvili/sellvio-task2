18dc2e7d517c57c7ed68b88a3e94dd82fd686de7
guja kupreishvili <gujakupreishvili4@gmail.com>
2025-09-07 13:47:30 +0400

    style:change style

--- src/app/components/content/content.tsx
+++ src/app/components/content/content.tsx
@@ -8,11 +8,11 @@ import ActiveCampaings from '../activeCampaigns/activeCampaings'
 
 export default function Content() {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col max-w-[1980px justify-center]'>
       <div className='flex flex-col mt-[64px] items-center mb-[64px]  '>
         <h1 className='text-[32px] font-manrope font-medium text-[#1E1E1E] mb-[28px]'>როგორ მუშაობს</h1>
 
-        <div className="w-[95%] h-[72px] bg-[#00000014] rounded-[12px] flex items-center px-[14px] py-[7px] mb-[28px]">
+        <div className="w-[95%] h-[72px] bg-[#00000014] rounded-[12px] flex items-center px-[14px] py-[7px] mb-[28px] max-w-[1980px]">
         <p className="w-[50%] text-center  py-[8px] rounded-[4px] text-[#3012B3CC] text-[16px] font-bold font-manrope ">
           ანალიტიკა
         </p>
@@ -31,7 +31,7 @@ export default function Content() {
 
       <div className='flex flex-col items-center mx-[17px] '>
         <h1 className=' text-[#583CCF] text-[32px] font-manrope font-bold mb-[31px]'>აღმოაჩინე აქტიური კამპანიები</h1>
-        <div className='flex flex-wrap w-full mb-[100px] gap-[26px] '>
+        <div className='flex flex-wrap xl:justify-center  w-full mb-[100px] gap-[26px] '>
           <ActiveCampaings  compaName='კარფური' rate='4.6' text='შექმენი ტრენდული ცონტენტი კარფურისთვის შენივე სტილით.' pricePer1k={28} currentOrders={18} totalOrders={25}/>
           <ActiveCampaings  compaName='სპარი' rate='4.9' text='შექმენი ტრენდული კონტენტი სპარისთვის შენივე სტილით სპარი განძი კარფური ბანძიშექმენი ტრენდული კონტენტი სპარისთვის შენივე სტილით სპარი განძი კარფური ბანძი.' pricePer1k={38} currentOrders={24} totalOrders={25}/>
           <ActiveCampaings  compaName='დეილი' rate='4.0' text='შექმენი ტრენდული ცონტენტი დეილისთვსი შენივე სტილით.' pricePer1k={18} currentOrders={18} totalOrders={25}/>
